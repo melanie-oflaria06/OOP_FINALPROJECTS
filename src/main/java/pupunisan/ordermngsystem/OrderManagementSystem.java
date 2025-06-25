14fa46da3661c4802228298339da21349266cab1
Admin <Admin@DESKTOP-EQ55Q8H>
2025-06-25 12:47:44 +0800

    First Commit

--- src/main/java/pupunisan/ordermngsystem/OrderManagementSystem.java
+++ src/main/java/pupunisan/ordermngsystem/OrderManagementSystem.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package pupunisan.finals_form.ordermngsystem;
+package pupunisan.ordermngsystem;
 
 import java.util.ArrayList;
 import javax.swing.DefaultListModel;
